8b0cbe831ab0867a8289bf6cccc8be5f2d349966
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-21 17:54:54 +0000

    container config done

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -8,7 +8,18 @@ console.log("shadab")
 let name = "shadab"
 let age = "19"
 let isLoggedIn = false
+let state;
 
 
 //number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>stanalone value
+//undefined =>
+//symbol =>unique 
 
+//object 
+
+console.log (typeof undefined);    //undefined
+console.log(typeof null);       //object
