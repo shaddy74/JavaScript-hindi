e08fc76abb89985a01a7c918ea690f7aedb35fad
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-21 17:30:11 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 122332
+let accountEmail = "sadab20012005@gmail.com"
+var accountPassword = "123123"
+accountCity = "Jaipur"
+
+//accountId = 2 // Not Allowed
+
+accountEmail = "shaddya976@gmail.com"
+accountPassword = "101010"
+accountCity = "Patna"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
