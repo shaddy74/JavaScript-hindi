79123299db0934c446ea15424ab2539b988fa454
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-21 17:42:04 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat All JS code as newer version
+
+//alert( 3 + 3) //we are using modernjs, not browser
+
+console.log("shadab")
+
+
+let name = "shadab"
+let age = "19"
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+
