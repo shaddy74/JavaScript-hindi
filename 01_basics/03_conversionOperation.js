2e0f2e4ef02c1d73523e3ee0d42ed7b571dab3f6
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-22 05:38:59 +0000

    conversion of dataType done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => easily convert to 33
+//"33abc" => convert to NaN(Not a Number)
+//"true" => 1;
+//"false" => 0
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//1 => true; 0 => false
+//"" => false
+//"shadab" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
