a2a83a191e868b69571790eadcb96fa190a74e6c
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-22 06:12:56 +0000

    cnversion of dataType Operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,13 @@
+//*************************  Conversions  ********************************
+
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => easily convert to 33
@@ -17,7 +19,7 @@ console.log(valueInNumber);
 let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 //1 => true; 0 => false
@@ -27,5 +29,25 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************************  Operations  ********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " bhai"
+let str3 = str1 + str2
+// console.log(str3);
+
