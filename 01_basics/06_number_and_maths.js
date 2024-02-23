4b1328ada8032cea5ba42222ca89be57bb519475
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-23 11:00:15 +0000

    math and number

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8656
+console.log(otherNumber.toPrecision(3))
+
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'))
