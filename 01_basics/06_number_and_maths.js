57193118560770b34428504e85a315f759fcb513
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-23 18:33:37 +0000

    Math and Number done

--- 01_basics/06_number_and_maths.js
+++ 01_basics/06_number_and_maths.js
@@ -1,15 +1,39 @@
 const score = 400
-console.log(score)
+//console.log(score)
 
 const balance = new Number(100)
-console.log(balance)
+//console.log(balance)
 
-console.log(balance.toFixed(2));
+//console.log(balance.toFixed(2));
 
 
 const otherNumber = 123.8656
-console.log(otherNumber.toPrecision(3))
+//console.log(otherNumber.toPrecision(3))
 
 
 const hundred = 1000000
-console.log(hundred.toLocaleString('en-IN'))
+//console.log(hundred.toLocaleString('en-IN'))
+
+//  *************  Maths   *************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.8));
+
+// console.log(Math.min(8,7,5,2,3,5,4,));
+// console.log(Math.max(7,2,6,1,4,8,9));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+Math.random()
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
