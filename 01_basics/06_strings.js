f93e9f34a3021db20a9153fc749801cbc103134b
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-23 06:55:12 +0000

    string explore pending...

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,93 @@
+const name = "shadab"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo coount is ${repoCount}`);
+
+const gameName = new String('shaddy-a-com')
+
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('d'))
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-7, 5)
+console.log(anotherString);
+
+const newStringOne = "  shadab  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+const url = "https://shaddy.com/shaddy%30abc"
+
+console.log(url.replace('%30', '-'))
+
+console.log(url.includes('shaddy'))
+
+console.log(gameName.split('-'));
+
+
+
+// anchor:
+// at:
+// big:
+// blink:
+// bold:
+// charAt:
+// charCodeAt:
+// codePointAt:
+// concat:
+// constructor:
+// endsWidth:
+// fixed:
+// fontcolor:
+// fontsize:
+// includes:
+// indexOf:
+// italics:
+// lastIndexOf:
+// lenght:
+// link:
+// localeCompare:
+// match:
+// matchAll:
+// normalize:
+// padEnd:
+// padStart:
+// repeat:
+// replace:
+// replaceAll:
+// search:
+// slice:
+// small:
+// split:
+// startsWith:
+// strike:
+// sub:
+// substr:
+// substring:
+// sup:
+// toLocaleLowerCase:
+// toLocaleUpperCase:
+// toLowerCase:
+// toString:
+// toUpperCase:
+// trim:
+// trimEnd:
+// trimLeft:
+// trimRight:
+// trimStart:
+// valueOf:
