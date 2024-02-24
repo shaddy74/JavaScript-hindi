58fd1a18524c6367310fe1c06595ae96ef3e0804
JavaScript-Beginner <149529893+shaddy74@users.noreply.github.com>
2024-02-24 04:57:01 +0000

    date and time done

---
+++ 01_basics/08_datetime.js
@@ -0,0 +1,24 @@
+//  Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+//let myCreateDate = new Date(2024, 3, 14)
+//console.log(myCreateDate.toDateString());
+//let myCreatedDate = new Date("2024-03-11")  //yyyy/mm/dd
+let myCreatedDate = new Date("03-11-2024")    //mm/dd/yyyy
+//console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
