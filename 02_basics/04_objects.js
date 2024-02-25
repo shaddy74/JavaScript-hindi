17bc728de6b44107435d1b40a8269318db7ce3ad
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-25 17:23:45 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,12 +47,36 @@ const users = [
         email: "sgay@gmail.com",
     }
 ]
-console.log(users[1].email)
-console.log(tinderUser);
+//console.log(users[1].email)
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));   // Important
-console.log(Object.values(tinderUser));   // Important
-console.log(Object.entries(tinderUser));   // Important
+// console.log(Object.keys(tinderUser));   // Important
+// console.log(Object.values(tinderUser));   // Important
+// console.log(Object.entries(tinderUser));   // Important
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
+const course = {
+    courseName : "js in hindi",
+    price : "999",
+    courseInstructure: "sadab"
+}
+// console.log(course.courseInstructure);
+
+const {courseInstructure: instructure} = course
+
+//console.log(courseInstructure);
+console.log(instructure)
+
+
+// {
+//     name: "shadab",
+//     course: "free",
+//     coursename: "js in hindi"
+// }
+
+
+
+
