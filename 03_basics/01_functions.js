a90115d5290167a5f542a92e46fe9faf46951660
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-26 04:39:27 +0000

    Function And Objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,7 +26,43 @@ const result = addTwoNumbers(3,5)
 
 
 function loginUserMassage(username){
+    if(username === undefined){
+        console.log("Pleanse enter username");
+        return
+    }
     return `${username} just logged in`
 }
 
-console.log(loginUserMassage("sadab"))
+//console.log(loginUserMassage("sadab"))
+//console.log(loginUserMassage(""))
+
+
+
+function calculateCartPrice (...num1) {
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500,100,500))
+
+
+const user = {
+    username: "shaddy",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`User name is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username:"sam",
+    price: 1999
+})
+
+
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondeValue(getArray){
+    return getArray[2]
+}
+console.log(returnSecondeValue(myNewArray));
