43e30fafb31a4872335d2561384d89fb27689027
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-26 06:30:14 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+//let a = 10
+const b = 20
+//var c = 30
+let a = 300
+
+if (true){
+    let a = 10
+    const b = 20
+    console.log("Inner:", a);
+}
+
+
+
+console.log(a)
+// console.log(a);
+// console.log(b);
+// console.log(c);
