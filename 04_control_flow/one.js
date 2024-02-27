9da017cb3ae238c7c40ac3b75c6d30dcaa19b0e1
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-27 13:51:08 +0000

    conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if statement
+
+const isUserloggedIn = true
+const temperature = 25
+
+if(temperature === 30){
+    console.log("less than...");
+} else{
+    
+    console.log("temperature is greater than 30");
+}
+
+// <, >, <=, >= ==, !=, ===, !==
