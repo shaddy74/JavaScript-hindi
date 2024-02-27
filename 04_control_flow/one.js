cc1e4c5ef8a4c1a51e09ea302131ff72a84cf3a0
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-27 14:29:33 +0000

    condition

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,13 +1,38 @@
 // if statement
 
-const isUserloggedIn = true
-const temperature = 25
+// const isUserloggedIn = true
+// const temperature = 31
 
-if(temperature === 30){
-    console.log("less than...");
-} else{
+// if(temperature === 30){
+//     console.log("less than...");
+// } else{
     
-    console.log("temperature is greater than 30");
-}
+//     console.log("temperature is greater than 30");
+// }
+
+// console.log("Execute");
 
 // <, >, <=, >= ==, !=, ===, !==
+
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`userpower: ${power}`);
+// }
+// console.log(`userpower: ${power}`);
+
+
+
+const balance = 1000
+if (balance < 500){
+    console.log("less than");
+} else if (balance < 750){
+    console.log("less than 750");
+} else if (balance < 900){
+    console.log("less than 900");
+} else{
+    console.log("less than 1200");
+}
