a8ce0c6437532d54c9839d15711934bc375deabc
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-28 05:44:37 +0000

    Switch Cases

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key){
+//    case value:
+
+//    break;
+
+//    default: 
+
+//    break;
+
+//    }
+
+
+const month = 7
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Febuary")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")
+        break;
+
+    default:
+        console.log("June-December")
+        break;
+}
