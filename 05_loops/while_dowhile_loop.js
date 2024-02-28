886c0f64b26cdc330d63eed40821083a63a0e989
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-28 18:16:07 +0000

    while & do while concept complete

---
+++ 05_loops/while_dowhile_loop.js
@@ -0,0 +1,24 @@
+//  While
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index ${index}` );
+//     index = index +1
+// }
+
+
+let myarray = ["flash", "batman", "superman"]
+let arr = 0
+while (arr < myarray.length){
+    console.log(`Aray value is: ${myarray[arr]}`);
+    arr = arr +1
+}
+
+
+// Do While loop
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
