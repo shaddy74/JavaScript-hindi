cb366692c275a71636eadd76dd25c357594e897d
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-28 05:44:18 +0000

    Condition complete

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -26,13 +26,45 @@
 
 
 
-const balance = 1000
-if (balance < 500){
-    console.log("less than");
-} else if (balance < 750){
-    console.log("less than 750");
-} else if (balance < 900){
-    console.log("less than 900");
-} else{
-    console.log("less than 1200");
+// const balance = 1000
+// if (balance < 500){
+//     console.log("less than");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900){
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy Course");
 }
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User LoggedIn")
+}
+
+// Nillish Coalesing Operator (??): null undefined,
+
+let val1;
+//val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1)
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
