052b250948987f1d07dc973bd548072513aa60b8
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-02-29 04:45:41 +0000

    for in loop decribe

---
+++ 05_loops/04_for_in_loop.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcur is for: ${myObject[key]}`);
+}
+
+const programming = ["js", "ruby", "cpp", "py"]
+
+for (const key in programming) {
+    // console.log(programming[key])
+
+}
+
+
