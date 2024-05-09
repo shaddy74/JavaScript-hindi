9fdeebdb9a815526b42cd443c909b24ecda685a5
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-05-09 10:55:57 +0000

    Classe And Opp's Discussion

--- 08_classes_and_oops/myClasses.js
+++ 08_classes_and_oops/myClasses.js
@@ -1,15 +1,42 @@
 // ES6
 
-class User{
-    contructure(username, email, password){
-        this.email = email;
-        this.password = password;
-    }
+// class User{
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password
+//     }
+//     encrypyPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
 
-    encrypyPassword(){
-        return `${this.password}abc`
-    }
+
+// const chai = new User("shaddy", "shaddy@gmail.com", "123")
+// console.log(chai.encrypyPassword());
+// console.log(chai.changeUsername());
+
+
+// behind the scene 
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encrypyPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
 }
 
 
-const chai = new User("shaddy", "shaddy@gmail.com", "123")
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encrypyPassword());
+console.log(tea.changeUsername());
