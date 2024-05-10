6e0cfb1472d51c0efc318df11c3133988c1366f8
Md. Sadab Alam <149529893+shaddy74@users.noreply.github.com>
2024-05-10 06:21:16 +0000

    incomplete getter and setter

---
+++ 08_classes_and_oops/getter_setter.js
@@ -0,0 +1,21 @@
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+
+
+    
+
+    get password(){
+        return `${this._password}shadab`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const shadab = new User ("sadab20012005@gamil.com", "abc")
+console.log(shadab.password);
